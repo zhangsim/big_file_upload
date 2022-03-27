a9969b5780feef92b2d363d2ebe5dca829255973
zhangsiming <zhangsiming@yy.com>
2022-03-27 14:10:41 +0800

    ii

---
+++ server/node-server/test.js
@@ -0,0 +1,5 @@
+const path = require("path");
+// const UPLOAD_DIR = path.resolve(__dirname, "..", "target"); // 大文件存储目录
+console.log('路径'+path.resolve(__dirname, "../target"))
+console.log('路径'+path.resolve(__dirname, "./target"))
+console.log('路径'+path.resolve(__dirname, "..", "target"))
